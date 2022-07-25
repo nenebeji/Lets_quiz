81e567620ff10b3517bb8c532cfe28cac1d2ffec
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-07-25 17:35:19 +0100

    added quizdetails var

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+var startquiz_button = document.getElementById("StartQuiz");
+var quiz = document.getElementById("quiz")
+
+var quizdetails = [
+    {
+        quesion: "Commonly used data types are _________.",
+        1: "strings, numbers and HTML",
+        2: "strings, numbers and booleans",
+        3: "numbers and cascading",
+        4: "python",
+        correct: "2",
+    },
+    {
+        quesion: "Arrays in JavaScript can be used to store __________.",
+        1: "boleans",
+        2: "numbers",
+        3: "strings",
+        4: "all of the above",
+        correct: "4",
+    },
+    {
+        quesion: "The condition in an if / else statement is enclosed with ___________.",
+        1: "parenthesis",
+        2: "quotes",
+        3: "curly brackets",
+        4: "square brackets",
+        correct: "3",
+    },
+    {
+        quesion: "A useful tool used during development and debugging for printing content to the debugger is: ",
+        1: "console.log",
+        2: "terminal/bash",
+        3: "queryselector",
+        4: "for loops",
+        correct: "1",
+    },
+];
