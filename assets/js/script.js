f908f17750e672feb1b3e5a4b7ba6627972fa9cf
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-07-27 16:28:31 +0100

    restarted a new code so updated all htmls and added an intro page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,129 +1,146 @@
-var quiz = document.getElementById("quiz");
-// make answers an array
-var answer = document.querySelector("#answer");
-var questions = document.querySelector("#question");
-var ansOne = document.getElementById("1");
-var ansTwo = document.getElementById("2");
-var ansThree = document.getElementById("3");
-var ansFour = document.getElementById("4");
-var end = document.getElementById("end-container")
+var quizcontainer = document.getElementById("container");
+var endcontainer = document.getElementById("end-container")
 var submitbtn = document.getElementById("submit");
-var startquizBtn = document.querySelector("#StartQuiz");
-var quizintro = document.getElementById("quiz-intro");
+var timeEl = document.querySelector("#timer");
 var finalscoretxt = document.querySelector("#final-score");
 
-var quizdetails = [
+var question = document.getElementById("question");
+var options = document.getElementsByClassName("option-text");
+var opt1 = document.getElementById("opt1");
+var opt2 = document.getElementById("opt2");
+var opt3 = document.getElementById("opt3");
+var opt4 = document.getElementById("opt4");
+var optionEL = document.querySelector("#optionEl");
+console.log(options);
+
+
+var acceptedAns = true;
+var questioncount = 0;
+
+var availableQs = [];
+
+var questions = [
     {
         question: "Commonly used data types are _________.",
-        1: "1. strings, numbers and HTML",
-        2: "2. strings, numbers and booleans",
-        3: "3. numbers and cascading",
-        4: "4. python",
-        correct: "2",
+        1: "strings, numbers and HTML",
+        2: "strings, numbers and booleans",
+        3: "numbers and cascading",
+        4: "python",
+        answer: 2
     },
     {
         question: "Arrays in JavaScript can be used to store __________.",
-        1: "1. boleans",
-        2: "2. numbers",
-        3: "3. strings",
-        4: "4. all of the above",
-        correct: "4",
+        1: "boleans",
+        2: "numbers",
+        3: "strings",
+        4: "all of the above",
+        answer: 4
     },
     {
         question: "The condition in an if / else statement is enclosed with ___________.",
-        1: "1. parenthesis",
-        2: "2. quotes",
-        3: "3. curly brackets",
-        4: "4. square brackets",
-        correct: "3",
+        1: "parenthesis",
+        2: "quotes",
+        3: "curly brackets",
+        4: "square brackets",
+        answer: 3
     },
     {
         question: "A useful tool used during development and debugging for printing content to the debugger is: ",
-        1: "1. console.log",
-        2: "2. terminal/bash",
-        3: "3. queryselector",
-        4: "4. for loops",
-        correct: "1",
+        1: "console.log",
+        2: "terminal/bash",
+        3: "queryselector",
+        4: "for loops",
+        answer: 1
     },
 ];
 
-console.log(quizdetails);
 
-var score = 0;
-var currentquiz = 0;
-var acceptedanswers = true;
 
-var timeEl = document.querySelector("#timer");
-
+// timer functions and variables
 var secondsLeft = 100;
+var timerInterval = null;
+
+timeout = setTimeout(setTimer, 1000);
 
 function setTimer() {
-    var timerInterval = setInterval(function() {
+    if(timerInterval) {
+        return;
+    }
+
+    timerInterval = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = secondsLeft 
     
         if(secondsLeft === 0) {
           // Stops execution of action at set interval
-          clearInterval(timerInterval)
+          resetTimer();
+          quizcontainer.style.display = "none";
+          endcontainer.style.display = "block";
+          finalscoretxt.textContent = secondsLeft;
         }
       }, 1000);
+
+}
+
+function stopTimer() {
+    clearInterval(timerInterval)
+    timerInterval = null;
+}
+
+function resetTimer() {
+    stopTimer();
+    secondsLeft = 0
+    timeEl.textContent = secondsLeft;
 }
 
 
-console.log(startquizBtn)
-console.log(quizintro)
-
-// adds event listener to start Quiz button
-startquizBtn.addEventListener("click", function(){
+// to start Quiz
+function startQuiz() {
     setTimer();
-    quizintro.style.display = "none";
-    quiz.style.display = "block";
-    
+    getnewQ();
 }
-);
 
-// selects message 
-var message = document.querySelector(".message");
+function getnewQ() {
 
-var h3 = document.querySelector("h3")
+    var currentQ = questions[questioncount];
+    question.innerText = currentQ.question;
+    opt1.innerText = currentQ[1];
+    opt2.innerText = currentQ[2];
+    opt3.innerText = currentQ[3];
+    opt4.innerText = currentQ[4];
+}
 
-    // set innerText fot html
-    function nextQ() {
-    var currentquizdetails = quizdetails[currentquiz];
-    questions.innerText = currentquizdetails.question;
-    ansOne.innerText = currentquizdetails[1];
-    ansTwo.innerText = currentquizdetails[2];
-    ansThree.innerText = currentquizdetails[3];
-    ansFour.innerText = currentquizdetails[4];
+
+startQuiz();
+getnewQ();
+// eventlistener for each click to switch to a question
+optionEL.addEventListener("click", function(event) {
+    // prevents bubbling when clicking on buttons
+    event.stopPropagation();
+    questioncount++
+
+    if(questioncount  < questions.length) {
+    getnewQ();
     }
+    else {
+        stopTimer();
+        quizcontainer.style.display = "none";
+        endcontainer.style.display = "block";
+        finalscoretxt.textContent = secondsLeft;
+    }
+    
+})
 
-    console.log(answer)
 
-   // executes nextQ function for Quiz questions
-    nextQ();
+// variable for each button according to data attribute converted to number to match the answer in each string
 
-    // add event listener for questions
-    answer.addEventListener("click", function(){
-        currentquiz++
-        if (currentquiz < quizdetails.length){
-        nextQ();
-        }
-        else {
-            quiz.style.display = "none";
-            end.style.display = "block";
-        }
-    })
+console.log(opt2.dataset["number"]);
 
-    console.log (quizdetails[currentquiz].correct)
+var opt1btn = + opt1.dataset["number"];
+var opt2btn = + opt2.dataset["number"];
+var opt3btn = + opt3.dataset["number"];
+var opt4btn = + opt4.dataset["number"];
+// get the answer in each string
+var correct = questions[questioncount].answer;
 
-   // function penalty(){
-      //  if(answer === quizdetails[currentquiz].correct) {
-        //    message.textContent = "Correct!";
-          //  h3.style.display = "block"
-          //  }
-          //  else {
-           //     message.textContent = "Wrong!";
-           //     h3.style.display = "block"
-           //         secondsLeft = secondsLeft - 10;
-           // }
-       // }
+console.log(opt2btn === correct)
