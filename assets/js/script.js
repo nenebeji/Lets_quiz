917b2eae649d6b037563c52ff2beda0fcf331fbc
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-07-28 11:40:34 +0100

    removed the previous alert message, was causing a lag in Highscore

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,7 +77,6 @@ function setTimer() {
         if(secondsLeft === 0) {
           // Stops execution of action at set interval
           resetTimer();
-          alert("OOOPS! TIME'S UP! GAME OVER!")
           quizcontainer.style.display = "none";
           endcontainer.style.display = "block";
           finalscoretxt.textContent = secondsLeft;
